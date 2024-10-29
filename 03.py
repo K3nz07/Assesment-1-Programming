54111b093acdd1baa4322b2fbb87e4ef98064174
Ranz <basti020613@gmail.com>
2024-10-29 10:41:46 +0400

    Create 03.py

---
+++ 03.py
@@ -0,0 +1,9 @@
+# Exercise 3 Writing a dictionary
+
+name = input('Type in your name')
+hometown = input('Type in your hometown')
+age = input('Type in your age')
+
+print(f'Your name is {name}')
+print(f'Your hometown is {hometown}')
+print(f'Your age is {age}')

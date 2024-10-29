e3f113905d488cca732ca8db756f2a86e80a8385
Ranz <basti020613@gmail.com>
2024-10-29 13:14:59 +0400

    Create 04.py

---
+++ 04.py
@@ -0,0 +1,41 @@
+# exercise 4 primitive quiz
+
+question = input('What is the capital of France').lower()
+if question == 'paris': print('you are correct')
+else: print('you are wrong')
+
+question2 = input('What is the capital of Czech Republic').lower()
+if question2 == 'prague': print('you are correct')
+else: print('you are wrong')
+
+question3 = input('What is the capital of Belgium ').lower()
+if question3 == 'brussels': print('you are correct')
+else: print('you are wrong')
+
+question4 = input('What is the capital of Denmark ').lower()
+if question4 == 'copenhagen': print('you are correct')
+else: print('you are wrong')
+
+question5 = input('What is the capital of Finland ').lower()
+if question5 == 'helsinki': print('you are correct')
+else: print('you are wrong')
+
+question6 = input('What is the capital of Germany ').lower()
+if question6 == 'berlin': print('you are correct')
+else: print('you are wrong')
+
+question7 = input('What is the capital of Greece ').lower()
+if question7 == 'athens': print('you are correct')
+else: print('you are wrong')
+
+question8 = input('What is the capital of Italy ').lower()
+if question8 == 'rome': print('you are correct')
+else: print('you are wrong')
+
+question9 = input('What is the capital of Luxembourg ').lower()
+if question9 == 'luxembourg city': print('you are correct')
+else: print('you are wrong')
+
+question10 = input('What is the capital of Netherlands ').lower()
+if question10 == 'amsterdam': print('you are correct')
+else: print('you are wrong')
